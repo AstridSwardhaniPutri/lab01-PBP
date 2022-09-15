df63311cb89ae8f57a6bf55cdade7abd1b962482
Astrid Swardhani Putri <astrid.swardhani@ui.ac.id>
2022-09-15 13:18:50 +0700

    updating json and xml type

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_xml 
+from wishlist.views import show_json
+from wishlist.views import return_json_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('json/<int:id>', return_json_id, name='return_json_id'),
 ]
