6b07958ce18d10ef67d658b39301c6c5b45a63d0
Astrid Swardhani Putri <astrid.swardhani@ui.ac.id>
2022-10-06 14:36:08 +0700

    lab 5

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist_ajax
 from wishlist.views import show_xml 
 from wishlist.views import show_json
 from wishlist.views import return_json_id
@@ -11,6 +12,7 @@ app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', return_json_id, name='return_json_id'),
